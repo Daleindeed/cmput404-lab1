a4f83973666c13a6e2d3b0255f758c64656afe00
Dale Yue <cadaleyu@gmail.com>
2021-09-08 13:55:12 -0600

    Modified code so that it downloads and prints itself but does not create a new python file

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import requests
 URL = "https://raw.githubusercontent.com/Daleindeed/cmput404-lab1/main/script.py"
 r = requests.get(URL)
-open('download.py', 'wb').write(r.content)
+# writing to a file with the content
+# discarded upon clarification from Abram
+# open('download.py', 'wb').write(r.content)
 print(r.content)

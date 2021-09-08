1b42f3c11f381017b02b01169ed264c9fbf6fb8a
Dale Yue <cadaleyu@gmail.com>
2021-09-08 11:25:16 -0600

    modified script.py so that it prints out its own source code from Github

--- script.py
+++ script.py
@@ -2,3 +2,4 @@ import requests
 URL = "https://raw.githubusercontent.com/Daleindeed/cmput404-lab1/main/script.py"
 r = requests.get(URL)
 open('download.py', 'wb').write(r.content)
+print(r.content)
